139b2dcdf7f9cec46d0a2e6759d898233ff4a97a
ViktorVan22 <zcg5368@163.com>
2022-07-25 11:09:36 +0800

    完成底部导航栏组件

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import { ConfigProvider } from "zarm";
 import zhCN from "zarm/lib/config-provider/locale/zh_CN";
 import "zarm/dist/zarm.css";
 
 import routes from "@/router";
+import NavBar from "@/components/NavBar";
+
 function App() {
+  const location = useLocation(); // 获取location实例
+  const { pathname } = location; // 获取当前实例
+  const needNav = ["/", "/data", "/user"];
+  const [showNav, setShowNav] = useState(false);
+
+  useEffect(() => {
+    setShowNav(needNav.includes(pathname));
+  }, [pathname]);
+
   return (
-    <Router>
+    <>
       <ConfigProvider primaryColor={"#007fff"} locale={zhCN}>
         <Routes>
           {routes.map(route => (
@@ -20,7 +31,8 @@ function App() {
           ))}
         </Routes>
       </ConfigProvider>
-    </Router>
+      <NavBar showNav={true} />
+    </>
   );
 }
 
