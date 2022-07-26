ed17d44b84bf370245f9cbbb31c6bf6ba49a5246
ViktorVan22 <zcg5368@163.com>
2022-07-26 20:18:58 +0800

    完成账单统计页面

--- src/container/Data/index.jsx
+++ src/container/Data/index.jsx
@@ -1,7 +1,133 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Icon, Progress } from "zarm";
+import dayjs from "dayjs";
+import cx from "classnames";
+
+import { get, typeMap } from "@/utils";
+import s from "./style.module.less";
+import PopupDate from "../../components/PopupDate";
+import CustomIcon from "../../components/CustomIcon";
 
 const Data = () => {
-  return <div>数据</div>;
+  const monthRef = useRef();
+  const [currentMonth, setCurrentMonth] = useState(dayjs().format("YYYY-MM"));
+  const [totalType, setTotalType] = useState("expense"); // 收入或支出类型
+  const [totalExpense, setTotalExpense] = useState(0); // 总支出
+  const [totalIncome, setTotalIncome] = useState(0); // 总收入
+  const [expenseData, setExpenseData] = useState([]); // 支出数据
+  const [incomeData, setIncomeData] = useState([]); // 收入数据
+
+  useEffect(() => {
+    getData();
+  }, [currentMonth]);
+
+  const monthShow = () => {
+    monthRef.current && monthRef.current.show();
+  };
+
+  const selectMonth = item => {
+    setCurrentMonth(item);
+  };
+
+  const getData = async () => {
+    const { data } = await get(`/api/bill/data?date=${currentMonth}`);
+
+    setTotalExpense(data.total_expense);
+    setTotalIncome(data.total_income);
+
+    const expense_data = data.total_data
+      .filter(item => item.pay_type == 1)
+      .sort((a, b) => b.number - a.number); // 过滤出账单类型为支出的项
+    const income_data = data.total_data
+      .filter(item => item.pay_type == 2)
+      .sort((a, b) => b.number - a.number); // 过滤出账单类型为收入的项
+    setExpenseData(expense_data);
+    setIncomeData(income_data);
+  };
+
+  // 切换收支构成类型
+  const changeTotalType = type => {
+    setTotalType(type);
+  };
+
+  return (
+    <div className={s.data}>
+      <div className={s.total}>
+        <div className={s.time} onClick={monthShow}>
+          <span>{currentMonth}</span>
+          <Icon className={s.date} type="date" />
+        </div>
+        <div className={s.title}>总支出</div>
+        <div className={s.expense}>￥ 1000</div>
+        <div className={s.income}>总收入￥200</div>
+      </div>
+      <div className={s.structure}>
+        <div className={s.head}>
+          <span className={s.title}>收支构成</span>
+          <div className={s.tab}>
+            <span
+              onClick={() => changeTotalType("expense")}
+              className={cx({
+                [s.expense]: true,
+                [s.active]: totalType === "expense",
+              })}
+            >
+              支出
+            </span>
+            <span
+              onClick={() => changeTotalType("income")}
+              className={cx({
+                [s.income]: true,
+                [s.active]: totalType == "income",
+              })}
+            >
+              收入
+            </span>
+          </div>
+        </div>
+        <div className={s.content}>
+          {(totalType === "expense" ? expenseData : incomeData).map(item => (
+            <div key={item.type_id} className={s.item}>
+              <div className={s.left}>
+                <div className={s.type}>
+                  <span
+                    className={cx({
+                      [s.expense]: totalType === "expense",
+                      [s.income]: totalType === "income",
+                    })}
+                  >
+                    <CustomIcon
+                      type={item.type_id ? typeMap[item.type_id].icon : 1}
+                    />
+                  </span>
+                  <span className={s.name}>{item.type_name}</span>
+                </div>
+                <div className={s.progress}>
+                  ￥{Number(item.number).toFixed(2) || 0}
+                </div>
+              </div>
+              <div className={s.right}>
+                <div className={s.percent}>
+                  <Progress
+                    shape="line"
+                    percent={Number(
+                      (item.number /
+                        Number(
+                          totalType == "expense" ? totalExpense : totalIncome
+                        )) *
+                        100
+                    ).toFixed(2)}
+                    theme="primary"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <PopupDate ref={monthRef} mode="month" onSelect={selectMonth} />
+    </div>
+  );
 };
 
 export default Data;
